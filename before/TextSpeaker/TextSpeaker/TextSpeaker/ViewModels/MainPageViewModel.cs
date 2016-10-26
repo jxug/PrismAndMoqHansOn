293488048a33f64d3ae5b6d3749ea60f1146ff85
nuits.jp <nuits.jp@live.jp>
2016-10-26 17:10:27 +0900

    Add Using Task.

--- before/TextSpeaker/TextSpeaker/TextSpeaker/ViewModels/MainPageViewModel.cs
+++ before/TextSpeaker/TextSpeaker/TextSpeaker/ViewModels/MainPageViewModel.cs
@@ -4,6 +4,7 @@ using Prism.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace TextSpeaker.ViewModels
 {
--- before/TextSpeaker/TextSpeaker/TextSpeaker/ViewModels/TextSpeachPageViewModel.cs
+++ before/TextSpeaker/TextSpeaker/TextSpeaker/ViewModels/TextSpeachPageViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Input;
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
 using Prism.Commands;
 using TextSpeaker.Model;
 using Xamarin.Forms;
